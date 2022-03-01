83727bc2f30e34440c14bfad1b10a45d16186db4
matildamort <matilda.jane.morton@gmail.com>
2022-03-01 18:20:52 +1000

    Added order controller and order-items controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,13 @@ class ProductsController < ApplicationController
     def index
     end
 
+    def show 
+        @product = Product.find(params[id])
+    end
+
+    def new
+        @prodcut = Product.new
+    end
+    
+
 end
