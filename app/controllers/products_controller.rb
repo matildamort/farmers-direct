9d7aee1b0d6a124e55f397360cc54d7665f8d5c5
matildamort <matilda.jane.morton@gmail.com>
2022-03-03 19:18:37 +1000

    added links to delete movie in show view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,15 +23,25 @@ class ProductsController < ApplicationController
         redirect_to products_path
        
     end
+
+    def delete
+
+    end
+
+    def edit
     
-    
+    end
+
+
     def product_params
         params.require(:product).permit(:name, :description, :price, :stock, :productpic, :user_id)
     end
 
+    
+
     def isFarmer
         if !current_user.farmer or !current_user.admin
-            redirect_to products_path, alert "You must be a registered farmer to add a product"
+            redirect_to products_path, alert: "You must be a registered farmer to add a product"
         end
     end
 
