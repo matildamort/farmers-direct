58bc4d03ad1f15cb58c3354d7513f9b38b00a03b
matildamort <matilda.jane.morton@gmail.com>
2022-03-03 15:29:34 +1000

    added picture upload to file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
     
     
     def product_params
-        params.require(:product).permit(:name, :description, :price, :stock, :user_id)
+        params.require(:product).permit(:name, :description, :price, :stock, :productpic, :user_id)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_191413) do
+ActiveRecord::Schema.define(version: 2022_03_03_020544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2022_03_02_191413) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "farmers", force: :cascade do |t|
+    t.string "name"
+    t.string "abn"
+    t.string "about"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_farmers_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -71,5 +81,6 @@ ActiveRecord::Schema.define(version: 2022_03_02_191413) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "farmers", "users"
   add_foreign_key "products", "users"
 end
